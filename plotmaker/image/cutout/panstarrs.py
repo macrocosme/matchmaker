3ee2315efeb9b2401ddef9e6cf5a7aa91db06940
Dany Vohl <d.vohl@uva.nl>
2023-07-26 13:01:50 +0200

    Make panstarss image function bit more general

--- plotmaker/image/cutout/panstarrs.py
+++ plotmaker/image/cutout/panstarrs.py
@@ -78,3 +78,62 @@ def download_images(obj1, obj2, mask1=None, mask2=None,
                     del obj1.matches[obj2.name].missing_images[np.where(obj1.matches[obj2.name].missing_images == i)[0][0]]
 
 
+
+def download_images_basic(obj1, obj2,
+                    mask1=None,
+                    image_radii=16 * u.arcsec,
+                    stretch=16,
+                    base_url = 'http://ps1images.stsci.edu/cgi-bin',
+                    output_path='data/panstarrs/',
+                    only_missing=False):
+
+    matched_obj1 = obj1.df.iloc[mask1] if mask1 is not None else obj1.df
+
+    if not only_missing:
+        obj1.matches[obj2.name].missing_images = []
+
+    for i, obj1_row in tqdm(matched_obj1.iterrows(), total=len(matched_obj1)):
+        if only_missing:
+            if i in obj1.matches[obj2.name].missing_images:
+                download = True
+            else:
+                download = False
+        else:
+            download = True
+
+        if download:
+            ra = obj1_row[obj1.cols.ra.label]
+            dec = obj1_row[obj1.cols.dec.label]
+
+            size = arcsec_to_pixel(image_radii)
+            check_folder_exists_or_create(output_path, return_folder=False)
+
+            fname = wget.download("{}/ps1filenames.py?ra={:f}&dec={:f}".format(base_url, ra, dec),
+                                  out=check_folder_exists_or_create('tmp/'))
+
+            image = obj1.matches[obj2.name].images[i] = Image(source_catalog='panstarrs')
+
+            with open(fname, "r") as f:
+                for line in f.readlines()[1:]:
+                    w = line.strip("\n").split(" ")
+                    filter = w[4]
+                    red = w[7]
+                    image.filters[filter] = Column()
+                    image.filters[filter].remote_download = "{}/fitscut.cgi?" \
+                                                            "ra={:f}&" \
+                                                            "dec={:f}&" \
+                                                            "red={}&" \
+                                                            "wcs=true&" \
+                                                            "format=fits&" \
+                                                            "size={}".format(base_url, ra, dec, red, size)
+
+                    image.filters[filter].file_location = wget.download(
+                        remove_file_if_exists(image.filters[filter].remote_download, return_filename=True),
+                        out="{}{}.fits".format(output_path, i)
+                    )
+
+                remove_file_from_folder(fname)
+                if i in obj1.matches[obj2.name].missing_images:
+                    del obj1.matches[obj2.name].missing_images[np.where(obj1.matches[obj2.name].missing_images == i)[0][0]]
+
+
